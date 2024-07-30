de2c16b8983acdc2de086de0361b8392616785f6
Timur <Stanmr7n@gmail.com>
2024-07-30 14:17:12 +0700

    Refactored code in testdemoqa and added new directory

--- tests/test_demo_qa.py
+++ tests/test_demo_qa.py
@@ -1,20 +1,18 @@
-from selene import browser, have, by
+from selene import browser, have
 import os
 
 
-def test_demo_qa():
+def test_student_registration_form():
     browser.open('/automation-practice-form')
-    browser.should(have.title('DEMOQA'))
+    browser.element('.practice-form-wrapper').should(have.text('Student Registration Form'))
 
-    browser.element('#firstName').type('test')
-    browser.element('#lastName').type('test')
+    browser.element('#firstName').type('name')
+    browser.element('#lastName').type('surname')
     browser.element('#userEmail').type('test@mail.com')
 
-    browser.element('[for=gender-radio-1]').click()
-    browser.element('[for=gender-radio-2]').click()
-    browser.element('[for=gender-radio-3]').click()
+    browser.all('[name=gender]').element_by(have.value('Male')).element('..').click()
 
-    browser.element('#userNumber').type('1111111111')
+    browser.element('#userNumber').type('7917000000')
 
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__year-select').click().element('option[value="1990"]').click()
@@ -24,22 +22,18 @@ def test_demo_qa():
     browser.element("#subjectsInput").type("che")
     browser.element('#react-select-2-option-0').should(have.text('Chemistry')).click()
 
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('[for="hobbies-checkbox-3"]').click()
+    browser.all('[for^=hobbies-checkbox]').element_by(have.text('Sports')).click()
 
-    browser.element('#uploadPicture').send_keys(os.path.abspath('cat.png'))
+    browser.element('#uploadPicture').send_keys(os.path.abspath('resources/cat.png'))
 
     browser.element('#currentAddress').type('Penyimpanan Rekod Hiong Foong Sdn Bhd 266, Lorong 1/14Y')
 
-    browser.element('#react-select-3-input').type("Haryana").press_enter()
-    browser.element('#react-select-4-input').type("Karnal").press_enter()
-
+    browser.element('#state').click().element('#react-select-3-option-2').should(have.text('Haryana')).click()
+    browser.element('#city').click().element('#react-select-4-option-0').should(have.text("Karnal")).click()
     browser.element('#submit').click()
 
     browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
-
     browser.element('.table').all('td').even.should(have.exact_texts(
-        'test test', 'test@mail.com', 'Other', '1111111111', '01 June,1990', 'Chemistry', 'Sports, Reading, Music',
+        'name surname', 'test@mail.com', 'Male', '7917000000', '01 June,1990', 'Chemistry', 'Sports',
         'cat.png',
         'Penyimpanan Rekod Hiong Foong Sdn Bhd 266, Lorong 1/14Y', 'Haryana Karnal'))
