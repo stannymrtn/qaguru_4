01c5e9a8811a69b420d04f2c0975b25cb8a600c9
Timur <Stanmr7n@gmail.com>
2024-08-12 10:30:31 +0700

    Fixed one more

--- tests/test_allure_decorated_steps.py
+++ tests/test_allure_decorated_steps.py
@@ -11,36 +11,26 @@ def test_decorator_steps():
 
 
 @allure.step('Открываем github')
-
-
 def open_page():
-    browser.open('https://github.com')
+    browser.open('/')
 
 
 @allure.step('Поиск репозитория {repo}')
-
-
 def search_repository(repo):
     browser.element("[data-target='qbsearch-input.inputButtonText']").click()
     browser.element('#query-builder-test').send_keys(repo).press_enter()
 
 
 @allure.step('Переходим по ссылке на репозиторий {repo}')
-
-
 def click_to_link_repository(repo):
     browser.element(by.link_text(repo)).click()
 
 
 @allure.step('Перейти по вкладке issues')
-
-
 def click_to_issues():
     browser.element("#issues-tab").click()
 
 
 @allure.step('Проверяем наличие нужного текста {text}')
-
-
 def should_be_text(text):
     browser.element(by.partial_text(text)).should(be.visible)
