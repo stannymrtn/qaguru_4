4f4658d52679e81325a1df352d8350122a087df1
Timur <Stanmr7n@gmail.com>
2024-08-12 10:37:55 +0700

    Fixed one more

--- pages/form_page.py
+++ pages/form_page.py
@@ -4,62 +4,77 @@ from path_utils import get_resource_path
 
 
 class RegistrationPage:
-    def open(self):
+    @staticmethod
+    def open():
         browser.open('/automation-practice-form/')
         browser.element('.practice-form-wrapper').should(have.text('Student Registration Form'))
 
-    def type_first_name(self, value):
+    @staticmethod
+    def type_first_name(value):
         browser.element('#firstName').type(value)
 
-    def type_last_name(self, value):
+    @staticmethod
+    def type_last_name(value):
         browser.element('#lastName').type(value)
 
-    def type_email(self, email):
+    @staticmethod
+    def type_email(email):
         browser.element('#userEmail').type(email)
 
-    def type_birthday(self, month, year, day):
+    @staticmethod
+    def type_birthday(month, year, day):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').type(month)
         browser.element('.react-datepicker__year-select').type(year)
-        browser.element(f'.react-datepicker__day--0{int(day):02d}:not(.react-datepicker__day--outside-month)').click()
+        browser.element(f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)').click()
 
-
-    def click_gender(self, gender):
+    @staticmethod
+    def click_gender(gender):
         browser.all('[name=gender]').element_by(have.value(gender)).element('..').click()
 
-    def type_number(self, phone):
+    @staticmethod
+    def type_number(phone):
         browser.element('#userNumber').type(phone)
 
-    def type_subjects(self, subjects):
+    @staticmethod
+    def type_subjects(subjects):
         browser.element('#subjectsInput').type(subjects).press_enter()
 
-    def click_hobby(self, hobby):
+    @staticmethod
+    def click_hobby(hobby):
         browser.all('[for^=hobbies-checkbox]').element_by(have.text(hobby)).click()
 
-    def select_picture(self, file):
+    @staticmethod
+    def select_picture(file):
         browser.element('[type="file"]').send_keys(get_resource_path(file))
 
-    def type_address(self, address):
+    @staticmethod
+    def type_address(address):
         browser.element('#currentAddress').type(address)
 
-    def type_state(self, state):
+    @staticmethod
+    def type_state(state):
         browser.element('#state').click()
         browser.all('[id^=react-select][id*=option]').element_by(
             have.exact_text(state)
         ).click()
 
-    def type_city(self, city):
+    @staticmethod
+    def type_city(city):
         browser.element('#city').click().element('#react-select-4-option-0').should(have.text(city)).click()
 
-    def click_submit(self):
+    @staticmethod
+    def click_submit():
         browser.element('#submit').click()
 
-    def should_text(self, text):
+    @staticmethod
+    def should_text(text):
         browser.element('[id=example-modal-sizes-title-lg]').should(
             have.text(text)
         )
 
-    def should_exact_text(self, first_name, email, gender, number, birthday, subject, hobbies, picture, address, city):
+    @staticmethod
+    def should_exact_text(first_name, email, gender, number, birthday, subject, hobbies, picture, address, city):
         browser.element('.table').all('td').even.should(
             have.exact_texts(first_name, email, gender, number,
                              birthday, subject, hobbies, picture, address, city))
